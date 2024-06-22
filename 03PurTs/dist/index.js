673495cc5c00faaaa88f005f8b1e9221494592bf
shashikant88093 <shashi94goswami@gmail.com>
2024-06-22 23:16:18 +0530

    learing  Pure Ts

--- 03PurTs/dist/index.js
+++ 03PurTs/dist/index.js
@@ -1,2 +1,19 @@
 "use strict";
-console.log("Hello, world!");
+// class User {
+//   email: string;
+//   name: string;
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+// const user = new User("shashi94goswami@gmail.com", "Shashi");
+// console.log(user);
+// readonly city: string;
+class User {
+    constructor(city) {
+        this.city = "Delhi";
+        this.city = city;
+    }
+}
+const user = new User("Delhi");
--- 03PurTs/src/index.ts
+++ 03PurTs/src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello, world!");
+// class User {
+//   email: string;
+//   name: string;
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
+// const user = new User("shashi94goswami@gmail.com", "Shashi");
+
+// console.log(user);
+
+// readonly city: string;
+
+class User {
+  readonly city: string = "Delhi";
+  constructor(city: string) {
+    this.city = city;
+  }
+}
+
+const user = new User("Delhi");
